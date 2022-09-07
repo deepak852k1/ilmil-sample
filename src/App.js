405eb9b83f376f71aa3f5a0d28a96f4a1d3d2cce
deepak852k1 <deepak852k1@gmail.com>
2022-09-07 15:05:20 +0530

    beautify

--- src/App.js
+++ src/App.js
@@ -1,47 +1,38 @@
-import React, { useEffect } from "react";
+import React, {useEffect} from "react";
 import "./App.css";
 import Header from "./Header/Header";
 import Home from "./Home/Home";
 import Courses from "./Courses";
 import Footer from './Footer/Footer';
 
-
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import {BrowserRouter as Router, Switch, Route} from "react-router-dom";
 function App() {
-  return (
-    <Router>
-    <div className="App">
-     <Switch>
-      <Route path="/Courses">
-          <Header/>
-            <Courses/>
-            <Footer/>
-           
-      </Route>
-          <Route path="/Footer">
-            
-            <Footer />
-          </Route>
-          <Route path="/Checkout">
-            
-           
-           
-            
-          </Route>
-          
-            
-         
-     <Route path="/">
-            <Header/>
-            <Home/>
-            <Footer/>
-      </Route>
-    </Switch>
-    </div>
-    </Router>
-  );
-  
-}
+    return (
+        <Router>
+            <div className="App">
+                <Switch>
+                    <Route path="/Courses">
+                        <Header/>
+                        <Courses/>
+                        <Footer/>
 
+                    </Route>
+                    <Route path="/Footer">
+
+                        <Footer/>
+                    </Route>
+                    <Route path="/Checkout"></Route>
+
+                    <Route path="/">
+                        <Header/>
+                        <Home/>
+                        <Footer/>
+                    </Route>
+                </Switch>
+            </div>
+        </Router>
+    );
+
+}
 
 export default App;
